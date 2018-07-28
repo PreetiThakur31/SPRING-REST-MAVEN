f1d515d64c6a76376e7fd42fde177c82278bf7c8
PreetiThakur31 <dell@192.168.100.9>
2018-07-28 19:11:10 +0530

    Spring-Rest-Maven-CustomExceptionHandling

--- springrestEx/src/main/java/springrestEx/controller/CustomerController.java
+++ springrestEx/src/main/java/springrestEx/controller/CustomerController.java
@@ -1,8 +1,6 @@
 package springrestEx.controller;
 
 import java.util.List;
-
-import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import springrestEx.exceptionHandler.CustomerNotFoundException;
 import springrestEx.exceptionHandler.CustomerNotSavedException;
 import springrestEx.model.Customer;
